70bb915c973c880effc9d995c797088fbb1b195f
Michael Desa <mjdesa@gmail.com>
2015-02-06 14:53:43 -0800

    Add basic CRUD functionality for Tasks
    We had a really good time.

--- todo/config/routes.rb
+++ todo/config/routes.rb
@@ -6,5 +6,12 @@ Rails.application.routes.draw do
   get '/about', to: 'site#about'
 
   # Also just keep it RESTful
-  get '/tasks', to: 'tasks#index'
+  get '/tasks', to: 'tasks#index', as: 'tasks'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+  post "/tasks", to: "tasks#create"
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
